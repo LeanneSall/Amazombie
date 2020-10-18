17d6321407d82607d5b4e76d1c908d3a7dc98fa7
Patrick Fortaleza <patrickfortaleza@gmail.com>
2020-10-18 14:57:18 -0700

    timer

--- scripts/home.js
+++ scripts/home.js
@@ -11,3 +11,9 @@ document.getElementById('icon').addEventListener('click', (e) => {
         nav.classList.add('active');
     }
 });
+
+function handleTickInit(tick) {
+    Tick.count.down('2020' + '-12-1').onupdate = function(value) {
+        tick.value = value;
+    };
+}
