45d67170c0c26e24f199fe77c52f9aaabd76e18b
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-10-23 22:40:54 -0700

    Started draft for new form validation

--- scripts/shoppingcart.js
+++ scripts/shoppingcart.js
@@ -7,12 +7,12 @@
 
 // tQ: for form validation
 class infofield {
-    constructor(id) {
-        this.id = id;
+    constructor(id, label = "") {
         this.obj = document.getElementById(id);
         this.txt = this.obj.innerHTML.trim().replace( /(<([^>]+)>)/ig, '');
         this.edit = this.obj.isContentEditable;
         this.valid = true;
+        this.label = label;
     }
 }
 
@@ -45,81 +45,7 @@ function getInputs() {
     "address-line-cc-4"
     ];
 
-    var obj_array = [];
-
-    for (i = 0; i < fields.length; ++i) {
-        obj_array.push(new infofield(fields[i]));
-    }
-
-    return obj_array;
-}
-
-function toggleEditable(fields){
-
-    var pencilIcon = document.getElementsByClassName("icon-pencil-container")[0];
-
-    if (pencilIcon.classList.contains("icon-pencil-container-editmode")) {
-        pencilIcon.classList.remove("icon-pencil-container-editmode");
-    } else {
-        pencilIcon.classList.add("icon-pencil-container-editmode");
-        //greyOutButton();
-    }
-
-    fields.forEach(element => {
-        var field = document.getElementById(element.id);
-        if (field.isContentEditable) {
-            field.classList.remove("order-summary-row-editmode");
-            field.contentEditable = false;
-        } else {
-            field.classList.add("order-summary-row-editmode");
-            field.contentEditable = true;
-        }
-    });
-}
-
-function toggleCCaddress() {
-    var cbox = document.getElementById("useshipping");
-
-    var fromArray = [
-    "address-line-1", // Mr. Chris Redfield
-    "address-line-2", // 300 Unicorn Way
-    "address-line-3", // Raccoon City, YK
-    "address-line-4" // H0H0H0
-    ];
-
-    var toArray = [
-    "address-line-cc-1",  // Mr. Chris Redfield
-    "address-line-cc-2",  // 300 Unicorn Way
-    "address-line-cc-3",  // Raccoon City, YK
-    "address-line-cc-4"  // H0H0H0
-    ];
-
-    if (cbox.checked) {
-        document.getElementsByClassName("useShippingAddress-cc")[0].classList.remove("useShippingAddress-cc-invalid")
-        document.getElementById("useThisAddress-cc").classList.add("useThisAddress-cc-hidden")
-        for (i = 0; i < fromArray.length; ++i) {
-            document.getElementById(toArray[i]).innerHTML = new infofield(fromArray[i]).txt;
-        } 
-    } else {
-        document.getElementsByClassName("useShippingAddress-cc")[0].classList.add("useShippingAddress-cc-invalid")
-        document.getElementById("useThisAddress-cc").classList.remove("useThisAddress-cc-hidden")
-    }
-
-    passToEditable = []; 
-    for (i = 0; i < toArray.length; ++i) {
-        passToEditable.push(new infofield(toArray[i]));
-    }
-
-    toggleEditable(passToEditable);
-}
-
-function validateInputs(obj_array) {
-
-    document.getElementById("order-submit-button").classList.remove("disabled", "order-submit-button-disabled");
-
-    var isOk = true;
-
-    var placeholders = [
+    var labels = [
         "full name",
         "email address" ,     
         "address: full name", // Mr. Chris Redfield
@@ -136,64 +62,171 @@ function validateInputs(obj_array) {
         "postal code (format: A1A1A1)" // 123
     ]
 
+    let obj_map = new Map();
+
+    for (i = 0; i < fields.length; ++i) {
+        obj_map.set(fields[i], new infofield(fields[i]));
+        obj_map.get(fields[i]).label = labels[i];
+    }
+
+    return obj_map;
+}
+
+function toggleSummaryFields(fields){
+
+    var pencilIcon = document.getElementsByClassName("icon-pencil-container")[0];
+
+    if (!(pencilIcon.classList.contains("icon-pencil-container-locked"))) {
+
+        if (pencilIcon.classList.contains("icon-pencil-container-editmode")) {
+            pencilIcon.classList.remove("icon-pencil-container-editmode");
+            document.getElementById("useshipping").disabled = true;
+            validateInputs();
+            if (document.getElementById("useshipping")) {
+                var fromArray = [
+                "address-line-1", // Mr. Chris Redfield
+                "address-line-2", // 300 Unicorn Way
+                "address-line-3", // Raccoon City, YK
+                "address-line-4" // H0H0H0
+                ];
+
+                var toArray = [
+                "address-line-cc-1",  // Mr. Chris Redfield
+                "address-line-cc-2",  // 300 Unicorn Way
+                "address-line-cc-3",  // Raccoon City, YK
+                "address-line-cc-4"  // H0H0H0
+                ];
+                transferAddress(fromArray, toArray);
+            }
+        } else {
+            pencilIcon.classList.add("icon-pencil-container-editmode");
+            document.getElementById("useshipping").disabled = false;
+            greyOutButton();
+        }
+
+        toggleEditable(fields);
+    }
+}
+
+// function toggleCCaddress() {
+//     var cbox = document.getElementById("useshipping");
+
+//     var fromArray = [
+//     "address-line-1", // Mr. Chris Redfield
+//     "address-line-2", // 300 Unicorn Way
+//     "address-line-3", // Raccoon City, YK
+//     "address-line-4" // H0H0H0
+//     ];
+
+//     var toArray = [
+//     "address-line-cc-1",  // Mr. Chris Redfield
+//     "address-line-cc-2",  // 300 Unicorn Way
+//     "address-line-cc-3",  // Raccoon City, YK
+//     "address-line-cc-4"  // H0H0H0
+//     ];
+
+//     if (cbox.checked) {
+//         document.getElementsByClassName("useShippingAddress-cc")[0].classList.remove("useShippingAddress-cc-invalid")
+//         document.getElementById("useThisAddress-cc").classList.add("useThisAddress-cc-hidden")
+//         transferAddress(fromArray, toArray);
+//     } else {
+//         document.getElementsByClassName("useShippingAddress-cc")[0].classList.add("useShippingAddress-cc-invalid")
+//         document.getElementById("useThisAddress-cc").classList.remove("useThisAddress-cc-hidden")
+//         document.getElementsByClassName("icon-pencil-container")[0].classList.add("disabled", "icon-pencil-container-locked");
+//     }
+
+//     let passToEditable = new Map(); 
+//     for (i = 0; i < toArray.length; ++i) {
+//         passToEditable.set(toArray[i]);
+//     }
+
+//     toggleEditable(passToEditable);
+// }
+
+function toggleEditable(fields) {
+    for (let [key, value] of fields) {
+        var field = document.getElementById(key);
+        if (field.isContentEditable) {
+            field.classList.remove("order-summary-row-editmode");
+            field.contentEditable = false;
+        } else {
+            field.classList.add("order-summary-row-editmode");
+            field.classList.remove("summary-form-cc-grey");
+            field.contentEditable = true;
+        }
+    };
+}
+
+function transferAddress(from, to) {
+    for (i = 0; i < from.length; ++i) {
+        document.getElementById(to[i]).innerHTML = new infofield(from[i]).txt;
+    } 
+}
+
+function validateInputs(obj_map) {
+
+    document.getElementById("order-submit-button").classList.remove("disabled", "order-submit-button-disabled");
+
+    var isOk = true;
+
     // tQ: check for input lengths
-    for (i = 0; i < obj_array.length; ++i) {
-        document.getElementById(obj_array[i].id).classList.remove("order-summary-row-invalid");
-        if (obj_array[i].txt.length <= 1 || obj_array[i].txt=="Please enter your " + placeholders[i]) {
-            obj_array[i].valid = false;
+    for (let [key, value] of obj_map) {
+        document.getElementById(key).classList.remove("order-summary-row-invalid");
+        if (value.txt.length <= 1 || value.txt=="Please enter your " + value.label) {
+            value.valid = false;
         }
     }
 
     // tQ: full name must be two words
     const regexp_fname = new RegExp(/^[a-z,',-]+(\s)[a-z,',-]+$/);
-    if (!(regexp_fname.test(obj_array[0].txt.toLowerCase()))) {
-        obj_array[0].valid = false;
+    if (!(regexp_fname.test(obj_map.get("full-name").txt.toLowerCase()))) {
+        obj_map.get("full-name").valid = false;
     }
     
     // tQ: email must be valid
     const regexp_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!(regexp_email.test(String(obj_array[1].txt).toLowerCase()))) {
-        obj_array[1].valid = false;
+    if (!(regexp_email.test(String(obj_map.get("email-address").txt).toLowerCase()))) {
+        obj_map.get("email-address").valid = false;
     }
 
     // tQ: postal code must match pattern
     const regexp_postal = /[a-zA-Z][0-9][a-zA-Z](-| |)[0-9][a-zA-Z][0-9]/;
-    var postal = [5,12];
+    var postal = ["address-line-4","address-line-cc-4"];
     for (i = 0; i < postal.length; ++i) {
-        if (!(regexp_postal.test(String(obj_array[postal[i]].txt)))) {
-            obj_array[postal[i]].valid = false;
+        if (!(regexp_postal.test(obj_map.get(postal[i]).txt))) {
+            obj_map.get(postal[i]).valid = false;
         }
     }
 
     // tQ: credit card number must match pattern
     const regexp_cc1 = /^[0-9]{16}$/;
-    var testThis = String(obj_array[6].txt).replaceAll(" ","");
-    if (!(regexp_cc1.test(String(obj_array[6].txt).replaceAll(" ","")))) {
-        obj_array[6].valid = false;
+    var testThis = String(obj_map.get("cc-line-1").txt).replaceAll(" ","");
+    if (!(regexp_cc1.test(String(obj_map.get("cc-line-1").txt).replaceAll(" ","")))) {
+        obj_map.get("cc-line-1").valid = false;
     }
     // tQ: credit card exp date must be valid
     const regexp_cc2 = /^[0-9]{2}\/[0-9]{2}$/;
-    obj_array[7].valid = false;
-    if (regexp_cc2.test(String(obj_array[7].txt).replaceAll(" ",""))) {
-        var month = parseInt(String(obj_array[7].txt).substring(0,2));
-        var year = parseInt(String(obj_array[7].txt).substring(3));
+    obj_map.get("cc-line-2").valid = false;
+    if (regexp_cc2.test(String(obj_map.get("cc-line-2").txt).replaceAll(" ",""))) {
+        var month = parseInt(String(obj_map.get("cc-line-2").txt).substring(0,2));
+        var year = parseInt(String(obj_map.get("cc-line-2").txt).substring(3));
         var compdate = new Date((2000 + year), month, 0);
         
         if (month >= 1 && month <= 12 && compdate >= new Date()) {
-            obj_array[7].valid = true;
+            obj_map.get("cc-line-2").valid = true;
         }
     }
     // tQ: credit card ccv must match pattern
     const regexp_cc3 = /^[0-9]{3}$/;
-    if (!(regexp_cc3.test(String(obj_array[8].txt).replaceAll(" ","")))) {
-        obj_array[8].valid = false;
+    if (!(regexp_cc3.test(String(obj_map.get("cc-line-3").txt).replaceAll(" ","")))) {
+        obj_map.get("cc-line-3").valid = false;
     }
 
     // tQ: append classes
-    for (i = 0; i < obj_array.length; ++i) {
-        if (!(obj_array[i].valid)) {
-            document.getElementById(obj_array[i].id).innerHTML = "Please enter your " + placeholders[i];
-            document.getElementById(obj_array[i].id).classList.add("order-summary-row-invalid");
+    for (let [key, value] of obj_map) {
+        if (!(value.valid)) {
+            document.getElementById(key).innerHTML = "Please enter your " + value.label;
+            document.getElementById(key).classList.add("order-summary-row-invalid");
             isOk = false;
         }
     }
@@ -209,11 +242,13 @@ function greyOutButton() {
 }
 
 // tQ: default behaviour
-var allInputFields = document.getElementsByClassName("summary-form");
-var checkThis = function() { validateInputs(getInputs()) };
+// var allInputFields = document.getElementsByClassName("summary-form");
+// var checkThis = function() { validateInputs(getInputs()) };
 
-for (i = 0; i < allInputFields.length; ++i) {
-    allInputFields[i].addEventListener("blur", checkThis, false);
-}
+// for (i = 0; i < allInputFields.length; ++i) {
+//     allInputFields[i].addEventListener("blur", checkThis, false);
+// }
 
+// tQ: checkbox should be checked
 document.getElementById("useshipping").checked = true;
+document.getElementById("useshipping").disabled = true;
